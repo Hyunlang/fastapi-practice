69ef0f54a9e2df8dd6a4671793ce0fa7ff158a58
Hyunlang <hl.ban@haezoom.com>
2022-03-22 11:33:21 +0900

    update db info

--- app/core/config.py
+++ app/core/config.py
@@ -13,19 +13,19 @@ class Settings(BaseSettings):
     DB_ECHO: bool = True
     SQLALCHEMY_DATABASE_URI: Optional[PostgresDsn] = None
 
-    # @validator("SQLALCHEMY_DATABASE_URI", pre=True)
-    # def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
-    #     print('assemble_db_connection.v : ', v)
-    #     print('assemble_db_connection.values : ', values)
-    #     if isinstance(v, str):
-    #         return v
-    #     return PostgresDsn.build(
-    #         scheme="postgresql",
-    #         user=values.get("POSTGRES_USER"),
-    #         password=values.get("POSTGRES_PASSWORD"),
-    #         host= values.get("POSTGRES_SERVER"), #type:ignore
-    #         path=f"/{values.get('POSTGRES_DB') or ''}",
-    #     )
+    @validator("SQLALCHEMY_DATABASE_URI", pre=True)
+    def assemble_db_connection(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        print('assemble_db_connection.v : ', v)
+        print('assemble_db_connection.values : ', values)
+        if isinstance(v, str):
+            return v
+        return PostgresDsn.build(
+            scheme="postgresql",
+            user=values.get("POSTGRES_USER"),
+            password=values.get("POSTGRES_PASSWORD"),
+            host= values.get("POSTGRES_SERVER"), #type:ignore
+            path=f"/{values.get('POSTGRES_DB') or ''}",
+        )
 
 
 
